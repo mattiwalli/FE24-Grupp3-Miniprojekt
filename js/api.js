ad29bfa8717a8eaa6872b303a111f67200664951
mattiwalli <mattiwalli_2000@hotmail.com>
2025-03-19 11:49:58 +0100

    Fetching

--- js/api.js
+++ js/api.js
@@ -1,2 +1,12 @@
 const API_KEY = "2NFkSv2AVnmF3akthFLbiPYqyHe2wp";
 const API_URL = `https://www.amdoren.com/api/currency.php?api_key=${API_KEY}`;
+
+export const getCurrency = async () => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+};
