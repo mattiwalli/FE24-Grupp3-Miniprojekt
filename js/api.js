c6834a4293eeeed82a54f6ec3bae8e89739ce34d
Matti Walli <mattiwalli_2000@hotmail.com>
2025-03-20 10:37:37 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,12 +1,35 @@
 const API_KEY = "2NFkSv2AVnmF3akthFLbiPYqyHe2wp";
 const API_URL = `https://www.amdoren.com/api/currency.php?api_key=${API_KEY}`;
 
-export const getCurrency = async () => {
+
+
+// Funktion som konverterar en summa från en valuta till en annan
+export async function convertCurrency(amount, from, to) {
+  const url = `${API_KEY}&from=${from}&to=${to}&amount=${amount}`;
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(url);
     const data = await response.json();
+    
+    if (data.error) {
+      throw new Error(data.error_message || "Conversion error");
+    }
     return data;
   } catch (error) {
-    console.error("Error fetching data", error);
+    console.error("Error converting currency:", error);
+    throw error;
   }
-};
+}
+
+// Funktion som hämtar alla valutor
+export async function getAllCurrencies(){
+
+  try{
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data; 
+  } catch (error) {
+    console.error("Error Fetching currensies: ", error);
+  }
+}
+
+
