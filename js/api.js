0be005a3606a2927fc33fa2ed73f15606ddca7e4
Matti Walli <mattiwalli_2000@hotmail.com>
2025-03-20 15:50:27 +0100

    Tog bort dubbla body tag i html, vilket orsaka problem

--- js/api.js
+++ js/api.js
@@ -1,15 +1,13 @@
 const API_KEY = "2NFkSv2AVnmF3akthFLbiPYqyHe2wp";
 const API_URL = `https://www.amdoren.com/api/currency.php?api_key=${API_KEY}`;
 
-
-
 // Funktion som konverterar en summa från en valuta till en annan
-export async function convertCurrency(amount, from, to) {
-  const url = `${API_KEY}&from=${from}&to=${to}&amount=${amount}`;
+async function convertCurrency(amount, from, to) {
+  const url = `${API_URL}&from=${from}&to=${to}&amount=${amount}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
-    
+
     if (data.error) {
       throw new Error(data.error_message || "Conversion error");
     }
@@ -21,32 +19,33 @@ export async function convertCurrency(amount, from, to) {
 }
 
 // Funktion som hämtar alla valutor
-export async function getAllCurrencies(){
-
-  try{
+async function getAllCurrencies() {
+  try {
     const response = await fetch(API_URL);
     const data = await response.json();
-    return data; 
+    return data;
   } catch (error) {
     console.error("Error Fetching currensies: ", error);
   }
 }
 
 document.querySelector("form").addEventListener("submit", async (event) => {
- event.preventDefault();
- const amount = document.querySelector("#amount").value;
- const fromcurrency = document.querySelector("#fromcurrency").value;
- const toCurrency = document.querySelector("#tocurrency").value;
+  event.preventDefault();
+  const amount = document.querySelector("#amount").value;
+  const fromcurrency = document.querySelector("#fromCurrency").value;
+  const toCurrency = document.querySelector("#toCurrency").value;
 
- try{
-  const conversionData = await convertCurrency(amount, fromcurrency, toCurrency);
-  document.querySelector("#result").textContent = `Converted amount: ${conversionData.amount}`;
- } catch (error) {
-   document.getElementById("result").textContent = "Error converting currency";
-  console.error("Error converting currency: ", error);
- }
-
-})
-
-
-
+  try {
+    const conversionData = await convertCurrency(
+      amount,
+      fromcurrency,
+      toCurrency
+    );
+    document.querySelector(
+      "#result"
+    ).textContent = `Converted amount: ${conversionData.amount}`;
+  } catch (error) {
+    document.getElementById("result").textContent = "Error converting currency";
+    console.error("Error converting currency: ", error);
+  }
+});
